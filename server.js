7de387ba4b917cb5e60e2dea9dcf85258fa15fcb
islam-Attar <islamattar95@gmail.com>
2022-02-04 13:35:23 +0200

    Task13

--- server.js
+++ server.js
@@ -14,20 +14,26 @@ function Movies(id, title, release_date, poster_path, overview) {
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const pg = require("pg")
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(DATABASE_URL);
+app.use(express.json());
+
 
 const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT;
 
 // const jsonData = require("./Movie Data/data.json");
 
-app.listen(`${PORT}`, () => {
-  console.log(`Listen to port ${PORT}`);
-});
+
 
 // app.get('/', getMoviesHandler);
-// app.get('/favorite', welcomeToFavoriteHandler);
+app.get('/favorite', welcomeToFavoriteHandler);
 app.get("/trending", trendingHandler);
 app.get("/search", searchHandler);
+
+app.post("/addMovie", addMovieHandler);
+app.get("/getMovies", getFavMovieHandler);
 
 app.use(errorHandler);
 app.use("*", notFoundHandler);
@@ -44,6 +50,11 @@ function errorHandler(error, req, res) {
 
   res.status(500).send(err);
 }
+
+function welcomeToFavoriteHandler(req, res) {
+  return res.status(200).send("Welcome to Favorite Page");
+}
+
 
 function trendingHandler(req, res) {
   let allMovies = [];
@@ -88,14 +99,44 @@ function searchHandler(req, res) {
     });
 }
 
-// function  getMoviesHandler(req, res){
+function addMovieHandler(req,res){
+  let movie = req.body
+  
 
-//     let movies = new Movies(jsonData.title, jsonData.poster_path, jsonData.overview); ;
+  const sql = `INSERT INTO favMovies(title, release_date, poster_path, overview,comments) VALUES($1, $2, $3, $4, $5) RETURNING * ;`
+ 
 
-//     return res.status(200).json(movies);
-// };
+  let values = [movie.title, movie.release_date, movie.poster_path, movie.overview, movie.comments];
+  
+  client.query(sql, values).then((data) => {
+     
+      return res.status(201).json(data.rows);
+  }).catch(error => {
+      errorHandler(error, req, res);
+  })
+};
 
-function welcomeToFavoriteHandler(req, res) {
-  return res.status(200).send("Welcome to Favorite Page");
+ function getFavMovieHandler(req, res){
+  const sql = `SELECT * FROM favMovies`;
+  client.query(sql).then(data => {
+      return res.status(200).json(data.rows);
+  }).catch(error => {
+      errorHandler(error, req,res);
+  })
 }
 
+function  getMoviesHandler(req, res){
+
+    let movies = new Movies(jsonData.title, jsonData.poster_path, jsonData.overview); ;
+
+    return res.status(200).json(movies);
+};
+
+
+
+client.connect().then(() => {
+    
+  app.listen(PORT, () => {
+      console.log(`Listen to port ${PORT}`);
+  });
+});
